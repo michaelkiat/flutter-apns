ad1c854bc289442c294ba9c431f6c42117774a3b
sckoh <soocheng.koh@ifastfinancial.com>
2021-04-06 16:08:12 +0800

    update firebase_messaging version to ^9.1.0

--- lib/src/firebase_connector.dart
+++ lib/src/firebase_connector.dart
@@ -3,19 +3,43 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 
 class FirebasePushConnector extends PushConnector {
-  final _firebase = FirebaseMessaging();
+  final _firebase = FirebaseMessaging.instance;
+  MessageHandler _onBackgroundMessage;
 
   @override
   final isDisabledByUser = ValueNotifier(false);
 
+  Map<String, dynamic> _getMessageMap(RemoteMessage message) {
+    return {
+      'notification': {
+        'title': message?.notification?.title,
+        'body': message?.notification?.body,
+      },
+      'data': message.data,
+    };
+  }
+
+  Future<void> _firebaseMessagingBackgroundHandler(
+      RemoteMessage message) async {
+    _onBackgroundMessage(_getMessageMap(message));
+  }
+
   @override
   void configure({onMessage, onLaunch, onResume, onBackgroundMessage}) {
-    _firebase.configure(
-      onMessage: onMessage,
-      onLaunch: onLaunch,
-      onResume: onResume,
-      onBackgroundMessage: onBackgroundMessage,
-    );
+    _onBackgroundMessage = onBackgroundMessage;
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      onMessage(_getMessageMap(message));
+    });
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      final messageMap = _getMessageMap(message);
+      if (onLaunch != null) {
+        onLaunch(messageMap);
+      } else if (onResume != null) {
+        onResume(messageMap);
+      }
+    });
+
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
     _firebase.onTokenRefresh.listen((value) {
       token.value = value;
@@ -27,7 +51,7 @@ class FirebasePushConnector extends PushConnector {
 
   @override
   void requestNotificationPermissions() {
-    _firebase.requestNotificationPermissions();
+    _firebase.requestPermission();
   }
 
   @override
